856ae228a0dad73c3b157a04b110ba991d26e72f
waltermanias <wmanias@gmail.com>
2020-05-14 00:03:59 -0300

    Some comments

--- index.js
+++ index.js
@@ -34,9 +34,11 @@ class SignUpRouter {
 }
 
 // signup-usecase
-// domain
-const mongoose = require("mongoose");
-const AccountModel = mongoose.model("Account");
+// Domain: Use cases and entities
+// This layer contains your business logic and defines how the layer outside of it can interact with it.
+// Business logic is central to your application.
+// In other words, we will have here our use cases representation.
+// We can have here a representation of the events who can be fired from this layer.
 class SignUpUseCase {
   async signUp(email, password, repeatPassword) {
     if (password === repeatPassword) {
@@ -45,11 +47,22 @@ class SignUpUseCase {
   }
 }
 
+// Application layer
+// This layer orchestrates the use of the entities found in the Domain layer. It also adapts request from the Framework layer to the domain
+// layer by isitting between the two.
+
 // add-account-repo
-// infrastructure
+// Infrastructure
+// Ports and Adapters: We have here different libraries that we can change for another implementation.
+// For example, we could use MongoDB as database but we can choose between Mongoose or MongoDB as driver for connect with the database engine.
+const mongoose = require("mongoose");
+const AccountModel = mongoose.model("Account");
 class AddAccountRepository {
   async add(email, password) {
     const user = await AccountModel.create({ email, password });
     return user;
   }
 }
+
+// 1. Ports and Adapters
+// 2. Application
